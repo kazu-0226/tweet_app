a343916cb2cd43a54bdffd4105da8e7a436c92b2
kinebuchi_kazuki <kinebuchi_kazuki@kinebuchikazukinoMacBook-Air.local>
2020-07-21 18:44:38 +0900

    [Add]パスワードのハッシュ化

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
-    validates :password, presence:true
     attachment :profile_image
     has_many :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_20_112702) do
+ActiveRecord::Schema.define(version: 2020_07_21_080019) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2020_07_20_112702) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "profile_image_id"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
